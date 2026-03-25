ed3101f38b51a953140bcfa6a931edff6c269be6
Llyrus Bennett <llyrus@outlook.com>
2026-03-25 19:38:31 +0800

    First project commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ComplianceVault - Supplier Agreement Management");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+        System.out.println("App started successfully ｡◕‿‿◕｡");
+    }
+}
